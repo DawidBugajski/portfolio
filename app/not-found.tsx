5e2421d572639f793beb98d162bc51f4d65720c2
Dawid <dawidbugajski1996@gmail.com>
2024-03-18 20:58:22 +0100

    Change position glitch item

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,16 +2,16 @@ import Link from 'next/link';
 
 export default function NotFound() {
   return (
-    <div className='flex justify-center items-center flex-col h-full'>
+    <>
       <div className='glitch text-center' data-text='404'>
         404
       </div>
       <Link
-        className='text-4xl hover:text-green transition-colors duration-150'
+        className='text-center -translate-x-1/2 -translate-y-1/2 absolute top-1/2 left-1/2 text-4xl hover:text-green transition-colors duration-150'
         href='/'
       >
         Return Home
       </Link>
-    </div>
+    </>
   );
 }
